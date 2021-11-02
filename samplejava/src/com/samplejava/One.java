3dcd2c3c92579f16cbb59d2893f123343c0b627f
l <l@DESKTOP-QV559OK>
2021-11-02 01:52:41 -0700

    commiting java code

---
+++ samplejava/src/com/samplejava/One.java
@@ -0,0 +1,10 @@
+package com.samplejava;
+
+public class One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Executing One.java file");
+	}
+
+}
